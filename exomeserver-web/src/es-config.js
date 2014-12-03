823c7bb58c27778910a98cf28fe3557966da6fb5
Alejandro Alemán <alejandro.aleman.ramos@gmail.com>
2014-12-03 11:52:50 +0100

    Fixed MAF from static studies

--- exomeserver-web/src/es-config.js
+++ exomeserver-web/src/es-config.js
@@ -1,7 +1,6 @@
 EXOME_SERVER_HOST = "http://aaleman:8080/exomeserver/rest"
 CELLBASE_VERSION = "v3";
 CELLBASE_HOST = "http://www.ebi.ac.uk/cellbase/webservices/rest";
-// CELLBASE_HOST = "http://ws-beta.bioinfo.cipf.es/cellbase/rest";
 
 var AVAILABLE_SPECIES = {
     "text": "Species",
