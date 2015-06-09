7eac8b970e725c164a2e1d3acc34b1e73c5fbc4f
Alejandro Alemán <alejandro.aleman.ramos@gmail.com>
2015-06-09 16:03:11 +0200

    Minor changes

--- population-variant-server-web/src/pvs-config.js
+++ population-variant-server-web/src/pvs-config.js
@@ -1,4 +1,4 @@
-PVS_HOST = "http://test.babelomics.org/pvs/rest";
+PVS_HOST = "http://ws.bioinfo.cipf.es/pvs/rest";
 CELLBASE_VERSION = "v3";
 CELLBASE_HOST = "http://wwwdev.ebi.ac.uk/cellbase/webservices/rest";
 CELLBASE_HOST = "http://bioinfo.hpc.cam.ac.uk/cellbase/webservices/rest";
