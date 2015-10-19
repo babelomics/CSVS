2f6e2bf5c0dfdd840e8107be7abac74331c2684f
Alejandro Alemán <aaleman@cipf.es>
2015-10-19 14:59:39 +0200

    Updated Jsorolla to develop. Changed CEllbase URL to bioinfo.hpc.cam.ac.uk

--- population-variant-server-web/conf/config.js
+++ population-variant-server-web/conf/config.js
@@ -1,3 +1,3 @@
 PVS_HOST = "http://test.babelomics.org/pvsdev/rest";
 CELLBASE_VERSION = "v3";
-CELLBASE_HOST = "http://bioinfodev.hpc.cam.ac.uk/cellbase";
+CELLBASE_HOST = "http://bioinfo.hpc.cam.ac.uk/cellbase";
