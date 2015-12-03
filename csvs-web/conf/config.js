d2f6df5a85a7366d7b2e1fc89338ce6d0f040055
Alejandro Alemán <aaleman@cipf.es>
2015-12-03 15:25:47 +0100

    Added JsoApplicationBehavior. Added login system

--- csvs-web/conf/config.js
+++ csvs-web/conf/config.js
@@ -1,3 +1,4 @@
 CSVS_HOST = "http://ws1.babelomics.org/csvs/rest";
 CELLBASE_VERSION = "v3";
 CELLBASE_HOST = "http://bioinfo.hpc.cam.ac.uk/cellbase";
+OPENCGA_HOST = "http://test.babelomics.org/opencga-0.7-dev";
