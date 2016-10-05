c48ef6afc41b77f0d983d6ea794eaf45d3e220ef
Alejandro Alemán <alejandro.aleman.ramos@gmail.com>
2016-10-05 10:56:23 +0200

    Added Elixir logo and URI.js

--- csvs-web/conf/config.js
+++ csvs-web/conf/config.js
@@ -1,5 +1,6 @@
-CSVS_HOST = "http://ws1.babelomics.org/csvs/rest";
-CSVS_HOST = "http://test.babelomics.org/csvs_1.1.1/rest";
+//CSVS_HOST = "http://ws1.babelomics.org/csvs/rest";
+//CSVS_HOST = "http://test.babelomics.org/csvs_1.1.1/rest";
+CSVS_HOST = "http://ws.babelomics.org/csvs/rest";
 CELLBASE_VERSION = "v4";
 CELLBASE_HOST = "http://bioinfo.hpc.cam.ac.uk/cellbase";
 CELLBASE_HOST = "http://bioinfodev.hpc.cam.ac.uk/cellbase-dev-v4.0";
