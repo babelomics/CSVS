605da32b1e479b041887302e2cf4d4203f91390f
agaor <sgaort@gmail.com>
2017-08-31 09:39:21 +0200

    Change url to news servers in Seville. Now develop can download variants. Now you can send a email to developer

--- csvs-web/conf/config.js
+++ csvs-web/conf/config.js
@@ -1,10 +1,12 @@
-//CSVS_HOST = "http://ws1.babelomics.org/csvs/rest";
 //CSVS_HOST = "http://test.babelomics.org/csvs_1.1.1/rest";
-CSVS_HOST = "http://ws.babelomics.org/csvs/rest";
+//CSVS_HOST = "http://ws2.babelomics.org/csvs/rest";
+CSVS_HOST = "http://csvs-dev.clinbioinfosspa.es:8080/csvs/rest";
 CELLBASE_VERSION = "v4";
-CELLBASE_HOST = "http://bioinfo.hpc.cam.ac.uk/cellbase";
-CELLBASE_HOST = "http://bioinfodev.hpc.cam.ac.uk/cellbase-dev-v4.0";
+//CELLBASE_HOST = "http://bioinfo.hpc.cam.ac.uk/cellbase";
+//CELLBASE_HOST = "http://bioinfodev.hpc.cam.ac.uk/cellbase-dev-v4.0";
+CELLBASE_HOST = "http://ws.babelomics.org/cellbase";
 OPENCGA_HOST = "http://test.babelomics.org/opencga-0.7-dev";
-STEVIA_SERVER_HOST = "http://stvtest.babelomics.org";
+//STEVIA_SERVER_HOST = "http://stvtest.babelomics.org";
+STEVIA_SERVER_HOST = "http://ws.babelomics.org/teststv02";
 
 STV_APP="CSVS";
