54b417da3a8f2231c6b748f7445a11dddf76eb4e
Gema Roldán <gemaroldangonzalez@gmail.com>
2020-07-17 11:28:27 +0200

    web: Added description in tables head, added some links (gene,rs, clinvar,cosmic,transcript), new tooltips (highlights,select/deselect, study/population),
    download limited to 10 records, state init in tabs after search, new fields in send a email

--- csvs-web/conf/config.js
+++ csvs-web/conf/config.js
@@ -1,30 +1,35 @@
-//CSVS_HOST = "http://test.babelomics.org/csvs_1.1.1/rest";
-//CSVS_HOST = "http://ws2.babelomics.org/csvs/rest";
-//CSVS_HOST = "http://csvs-dev.clinbioinfosspa.es:8080/csvs/rest";
-CSVS_HOST = "http://csvs.clinbioinfosspa.es:8080/csvs/rest";
+
+CSVS_HOST = "";
 CELLBASE_VERSION = "v4";
-//CELLBASE_HOST = "http://bioinfo.hpc.cam.ac.uk/cellbase";
-//CELLBASE_HOST = "http://bioinfodev.hpc.cam.ac.uk/cellbase-dev-v4.0";
-//CELLBASE_HOST = "http://ws.babelomics.org/cellbase";
-CELLBASE_HOST = "http://cellbase.clinbioinfosspa.es/cb";
-OPENCGA_HOST = "http://test.babelomics.org/opencga-0.7-dev";
-//STEVIA_SERVER_HOST = "http://stvtest.babelomics.org";
-STEVIA_SERVER_HOST = "http://ws.babelomics.org/teststv02";
+
+
+
+CELLBASE_HOST = "";
+OPENCGA_HOST = "";
+
+STEVIA_SERVER_HOST = "";
 
 STV_APP="CSVS";
 
 // Version file download
-VERSION_FILE = "28/11/2017";
-CSVS_EMAIL='csvs@clinbioinfosspa.es';
+VERSION_FILE = "3 January 2020";
+CSVS_EMAIL='';
+
+CSVS_LIMIT_MAX=10;
 
 // Mail
-URL_MAIL='http://babemailer.clinbioinfosspa.es:8081/babemailer';
+URL_MAIL='';
 FROM_EMAIL='';
-TO_EMAIL='csvs';
-HOST='localhost';
+TO_EMAIL='';
+HOST='';
 PORT=25;
 SECURE=null;
 DEBUG=null;
 IGNORE_TLS=null;
 USER='';
 PASS='';
+
+TITLE_APP="Collaborative Spanish Variant Server";
+ACRONYM_APP ="CSVS";
+PHENOTYPE="Spanish";
+
